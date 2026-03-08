8917b0b74135ba11fc8bcd00d6b78e7d9ef38d04
abhishekhebbar173 <82367194+abhishekhebbar173@users.noreply.github.com>
2026-03-08 18:10:49 +0530

    Add new line to GitFirst.java

--- TestGitProject/src/module1/GitFirst.java
+++ TestGitProject/src/module1/GitFirst.java
@@ -5,6 +5,9 @@ public class GitFirst {
 	public static void main(String[] args)
 	{
 		System.out.println("First line added in workspace1");
+
+		System.out.println("New line added in GitHub");
 	}
 
 }
+
